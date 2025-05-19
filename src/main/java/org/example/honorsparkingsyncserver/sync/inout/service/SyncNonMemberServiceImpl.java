4df3ee90763538d602dec9cf0eec296af39451fd
Shihyun Han <devowl99@gmail.com>
2025-05-20 02:02:19 +0900

    feat: dto list retactoring

--- src/main/java/org/example/honorsparkingsyncserver/sync/inout/service/SyncNonMemberServiceImpl.java
+++ src/main/java/org/example/honorsparkingsyncserver/sync/inout/service/SyncNonMemberServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.honorsparkingsyncserver.sync.inout.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.honorsparkingsyncserver.sync.inout.domain.dto.SyncNonMemberListResponse;
 import org.example.honorsparkingsyncserver.sync.inout.domain.dto.SyncNonMemberRequest;
 import org.example.honorsparkingsyncserver.sync.inout.domain.dto.SyncNonMemberResponse;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,32 @@ import java.time.LocalDateTime;
 public class SyncNonMemberServiceImpl implements SyncNonMemberService {
 
     @Override
-    public SyncNonMemberResponse getParkingInfo(SyncNonMemberRequest request) {
+    public SyncNonMemberListResponse getParkingInfo(SyncNonMemberRequest request) {
+        List<SyncNonMemberResponse> entries = new ArrayList<>();
 
-        // 더미데이터
-        return SyncNonMemberResponse.builder()
+        // 차량 번호 외 전부 더미데이터
+        // 더미데이터 1
+        entries.add(SyncNonMemberResponse.builder()
+                .vehicleNumber(request.getVehicleNumber())
+                .parkingLotLocation("서울 강남구 테스트주차장")
+                .entryTime(LocalDateTime.now().minusMinutes(42))
+                .totalParkingMinutes(42)
+                .currentFee(1500)
+                .entryPhotoUrl("https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/2023_Hyundai_Avante_N_1.jpg/330px-2023_Hyundai_Avante_N_1.jpg")
+                .build());
+
+        // 더미데이터 2
+        entries.add(SyncNonMemberResponse.builder()
                 .vehicleNumber(request.getVehicleNumber())
                 .parkingLotLocation("세종시 조치원읍")
-                .entryTime(LocalDateTime.now().minusMinutes(60))
-                .totalParkingMinutes(60)
-                .currentFee(3000)
+                .entryTime(LocalDateTime.now().minusMinutes(95))
+                .totalParkingMinutes(95)
+                .currentFee(4200)
                 .entryPhotoUrl("https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/2023_Hyundai_Avante_N_1.jpg/330px-2023_Hyundai_Avante_N_1.jpg")
+                .build());
+
+        return SyncNonMemberListResponse.builder()
+                .parkingEntries(entries)
                 .build();
     }
 
